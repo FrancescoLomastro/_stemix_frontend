0e3d780daaf149af1c69ea6542c454f6a702fc3f
Francesco Gregorio <francescogregorio.lomastro@mail.polimi.it>
2025-12-16 20:03:58 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/lomastro/stemix_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lomastro.stemix_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [],
+      child: MaterialApp.router(
+        title: 'SteMix',
+        //routerConfig:
+      ),
+    );
+  }
+}
